22aae3a0438ad1b9f815cd11a233ba258da8ecef
bsosox <brunososo1@gmail.com>
2026-04-16 00:27:48 +0000

    hecho hasta leer config :0

--- client/src/client.c
+++ client/src/client.c
@@ -17,10 +17,11 @@ int main(void)
 	logger = iniciar_logger();
 
 	log_info(logger, "Hola! Soy un log");
+	log_info(logger, "regay");
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -28,9 +29,11 @@ int main(void)
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
-
+	ip = config_get_string_value(config,"IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
 	// Loggeamos el valor de config
-
+	log_info(logger, valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -57,14 +60,19 @@ int main(void)
 t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
-	nuevo_logger = log_create("tp0.log", "LOGGER_tp0", true, LOG_LEVEL_INFO);
+	nuevo_logger = log_create("cliente.log", "CLIENTE_LOGGER", true, LOG_LEVEL_INFO);
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-
+	nuevo_config = config_create("cliente.config");
+	if(nuevo_config == NULL)
+	{
+		perror("Error al intentar cargar config");
+		exit(EXIT_FAILURE);
+	}
 	return nuevo_config;
 }
 
@@ -99,4 +107,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	  log_destroy(logger);
+	  config_destroy(config);
 }
